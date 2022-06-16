c15d1d40a7e9c412f465ae2fd26926f98ee89254
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2022-06-16 23:08:24 +0530

    initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("api running");
+  res.send("API Running");
 });
 
 const PORT = process.env.PORT || 5000;
