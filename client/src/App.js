04fabd1048c9fe1f5c8cff65ba1689441d6b86f4
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2022-06-26 23:38:43 +0530

    Setting up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
+import "./App.css";
 import Navbar from "./Components/layout/Navbar";
 import Landing from "./Components/layout/Landing";
+import Register from "./Components/auth/Register";
+import Login from "./Components/auth/Login";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+      <section className="container">
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </section>
     </>
   );
 };
