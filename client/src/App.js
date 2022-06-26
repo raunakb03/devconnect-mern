3b2def159110fe102a54923bd87d25ce0344005b
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2022-06-26 23:04:17 +0530

    initializing react and creating navbar and landi g page

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+import Navbar from "./Components/layout/Navbar";
+import Landing from "./Components/layout/Landing";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
+};
+
+export default App;
