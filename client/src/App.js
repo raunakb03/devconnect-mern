9a946769a51a863e80dbd106eb42d12faa56c317
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2022-06-29 23:09:14 +0530

    implementing alert feature using redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
 import "./App.css";
 import Navbar from "./Components/layout/Navbar";
 import Landing from "./Components/layout/Landing";
 import Register from "./Components/auth/Register";
 import Login from "./Components/auth/Login";
+import Alert from "./Components/layout/Alert";
+
+//redux 
+import {Provider} from 'react-redux'
+import store from "./store";
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
-      <section className="container">
+      <Provider store= {store}>
+        <Navbar />
         <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Landing />} />
         </Routes>
-      </section>
+        <section className="container">
+          <Alert/>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </Provider>``
     </>
   );
 };
